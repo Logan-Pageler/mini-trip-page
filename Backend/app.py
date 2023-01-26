02c177e61649244bcef04e0509529f1dfe4a29e5
Logan Pageler <pagelerlogan@gmail.com>
2023-01-26 12:45:25 -0700

    Added Database

--- Backend/app.py
+++ Backend/app.py
@@ -2,10 +2,11 @@ import functools
 import json
 import os
 
-from flask import Flask, render_template, url_for, redirect, session
-from authlib.integrations.flask_client import OAuth
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, url_for, redirect, session, request
+from authlib.integrations.flask_client import OAuthError, OAuth
+import pymysql
 import os
+
 
 
 AUTH_REDIRECT_URI = os.environ.get("FN_AUTH_REDIRECT_URI", default=False)
@@ -14,10 +15,18 @@ CLIENT_ID = os.environ.get("FN_CLIENT_ID", default=False)
 CLIENT_SECRET = os.environ.get("FN_CLIENT_SECRET", default=False)
 CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 
+DB_HOST = os.environ.get("DB_HOST", default=False)
+DB_USERNAME = os.environ.get("DB_USERNAME", default=False)
+DB_PASSWORD = os.environ.get("DB_PASSWORD", default=False)
+DB_NAME = os.environ.get("DB_NAME", default=False)
+DB_PORT = int(os.environ.get("DB_PORT", default=False))
+
+db = pymysql.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, db=DB_NAME, port=DB_PORT)
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_ENDPOINT", default=False)
 app.secret_key = os.urandom(12)
 
-CORS(app, support_credentials=True)
 
 oauth = OAuth(app)
 oauth.register(
@@ -30,40 +39,58 @@ oauth.register(
     }
 )
 
+# allow access from origin
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  print('test')
   return response
-
-@app.route('/auth/')
-def index():
-    # if google_auth.is_logged_in():
-    #     user_info = google_auth.get_user_info()
-    #     return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
-
-    return session.get('token') if session.get('token') != None else 'You are not currently logged in.'
 
 @app.route('/auth/Login/')
 def google():
 
-
-    
-    return oauth.google.authorize_redirect(AUTH_REDIRECT_URI).get_data()
-    # return uri
+    return oauth.google.authorize_redirect(AUTH_REDIRECT_URI)
 
 @app.route('/auth/google/callback/')
-@cross_origin(supports_credentials=True)
 def google_auth():
     token = oauth.google.authorize_access_token()
     session['token'] = token
     
     print(" Google User ", token)
-    return redirect('http://localhost:3000?token=' + token['userinfo']['at_hash'] + '&name=' + token['userinfo']['name'])
+    return redirect('http://localhost:3000?token=' + token['userinfo']['email'] + '&name=' + token['userinfo']['name'])
 
-@app.route('/auth/getToken/')
-def get_token():
-    return session.get('token') if session.get('token') != None else 'false'
+
+@app.route('/api/getTable/')
+def get_table():
+    args = request.args
+    user = args.get('user')
+    if(user != None):
+        cur = db.cursor()
+        cur.execute('SELECT user_table FROM User_Data WHERE user = %s', user)
+        result = cur.fetchone()
+        if result == None:
+            return 'false'
+        print(result[0])
+        return result[0]
+
+    return 'false'
+
+@app.route('/api/setTable/', methods=['POST'])
+def set_table():
+    print('''
+        UPDATE INTO User_Data (user, user_table)
+        VALUES ('%s', '%s')
+    ''' %(request.args['user'], request.data.decode("utf-8")))
+    cur = db.cursor()
+    print(cur.execute('''
+        REPLACE INTO User_Data (user, user_table) VALUES (%s, %s)
+    ''', (request.args['user'], request.data)))
+
+    db.commit()
+    
+    return 'true'
+        
+
+
     
